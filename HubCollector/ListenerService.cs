cbac993fe42a84c2bd2ea2be5c930e6b49ff0886
Luke van der Hoeven <hungerandthirst@gmail.com>
2014-04-03 16:23:05 -0400

    change statsd client. working for cli client now

--- HubCollector/ListenerService.cs
+++ HubCollector/ListenerService.cs
@@ -10,7 +10,7 @@ namespace HubCollector
     public partial class ListenerService : ServiceBase
     {
         public HubConnection KlondikeConnection;
-        public MetricsConfig StatsdConfig;
+        public Statsd StatsdClient;
 
         public ListenerService()
         {
@@ -18,6 +18,7 @@ namespace HubCollector
             
             // ReSharper disable once DoNotCallOverridableMethodsInConstructor
             EventLog.Log = "HubCollectorLog";
+            StatsdClient = new Statsd("v1113.bfgdev.inside", 8125);
 
             KlondikeConnection = new HubConnection("https://nuget-rai.bfgdev.com/api/signalr")
             {
@@ -28,35 +29,30 @@ namespace HubCollector
             statusHubProxy.On("updateStatus",
                 status =>
                 {
-                    Console.WriteLine("Status: {0}, Total: {1}", status.synchronizationState, status.totalPackages);
+                    //Console.WriteLine("Status: {0}, Total: {1}", status.synchronizationState, status.totalPackages);
+                    EventLog.WriteEntry("Message from SignalR received.");
                     EventLog.WriteEntry("Status: {0}, Total: {1}", status.synchronizationState, status.totalPackages);
-                    Metrics.Gauge("count", (double)status.totalPackages);
+                    StatsdClient.LogGauge("nuget.rai.packageCount", (int)status.totalPackages);
                 }
             );
 
-            StatsdConfig = new MetricsConfig
-            {
-                StatsdServerName = "v1113.bfgdev.inside",
-                StatsdServerPort = 8125,
-                Prefix = "RAIKlondike"
-            };
         }
 
         protected override void OnStart(string[] args)
         {
             StartListener();
-            StatsdClient.Metrics.Configure(StatsdConfig);
             EventLog.WriteEntry("The service was started successfully.", EventLogEntryType.Information);
         }
 
         public async void StartListener()
         {
-            System.Diagnostics.Debugger.Launch();
+            EventLog.WriteEntry("Starting connection to Klondike...");
             await KlondikeConnection.Start();
         }
 
         protected override void OnStop()
         {
+            EventLog.WriteEntry("Stopping connection to Klondike...");
             KlondikeConnection.Stop();
             EventLog.WriteEntry("The service was stopped successfully.", EventLogEntryType.Information);
         }
